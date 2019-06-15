8ef457296442d9e55c935c0e11887335b00c372b
huai <yanghuai2012@outlook.com>
2019-06-15 17:15:32 +0800

    add share button. display detial forecast in a new activity.

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +28,8 @@ import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements ForecastAdapter.ForecastAdapterOnClickHandler {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private ForecastAdapter mForecastAdapter;
     private TextView mErrorMessageTextView;
@@ -60,9 +66,14 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.action_refresh){
+        int clickedId = item.getItemId();
+        if(clickedId == R.id.action_refresh){
             mForecastAdapter.setWeatherData(null);
             loadWeatherData();
+            return true;
+        }
+        else if(clickedId == R.id.action_map){
+            openLocationMap();
             return true;
         }
         else
@@ -86,9 +97,32 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
         asyncTask.execute(location);
     }
 
+
+    /* Open DetailActivity to display weather details*/
     @Override
     public void onClick(String weather) {
-        Toast.makeText(this, weather, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, weather);
+        startActivity(intent);
+    }
+
+
+    /* Method for opening map app to display location*/
+    public void openLocationMap(){
+        String location = "131 Regiment Square, Vancouver, Canada";
+        Uri.Builder builder = new Uri.Builder();
+        Uri uri = builder.scheme("geo")
+                .path("0,0")
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else{
+            Log.d(TAG, "openLocationMap: cannot open " + uri.toString() + " . No receiving apps.");
+        }
+
     }
 
     /* Async task class for querying weather data from API*/
@@ -129,4 +163,5 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
             }
         }
     }
+
 }
